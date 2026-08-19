ef34a82c92f59bd83f554b44416caeaa7fea96d6
Henry P <HJPrestwich@gmail.com>
2026-08-19 19:28:37 +0100

    99% sure the enemy actually works now YIPPEEEEEEEEE!!!!!!!!

--- mono/Entities/Enemy.cs
+++ mono/Entities/Enemy.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using mono.Main;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 
 namespace mono.Entities
@@ -15,80 +11,82 @@ namespace mono.Entities
         public string state;
         public Queue<Node> path = new Queue<Node>();
         public Node NextNode { get; private set; }
-        
-        public Enemy(Texture2D texture, Vector2 Position) : base(texture,  Position)
+
+        public Enemy(Texture2D texture, Vector2 Position) : base(texture, Position)
         {
             state = null;
             this.Speed = 3;
         }
-        
+
         public void CheckState()
         {
-            
+
         }
 
         public void SetPath(Player player, Graph graph)
         {
-            path = AStar.ASTAR(this.Position, player.Position, graph);   
+            path = AStar.ASTAR(this.Position, player.Position, graph);
         }
         public override void Move(Player p)
         {
-            Vector2 OldLocation = Position;
-            Vector2 NewLocation = Position + CalcMove(p);
-
-            Rectangle PRect = p.Rectangle;
-
-            Rectangle ERect = RectCalc(NewLocation, Size);
-
-            if (!ERect.Intersects(PRect))
+            if (path != null && path.Count > 0)
             {
-                this.Position = NewLocation;   
+                Node next = path.Peek();
+
+                Vector2 OldLocation = Position;
+                Vector2 NewLocation = Position + CalcMove(next);
+
+                Rectangle PRect = p.Rectangle;
+
+                Rectangle ERect = RectCalc(NewLocation, Size);
+
+                if (!ERect.Intersects(PRect))
+                {
+                    Vector2 distanceV = next.Position - this.Position;
+                    double distanceD = Math.Sqrt(Math.Pow(distanceV.X, 2) + Math.Pow(distanceV.Y, 2));
+                    if (distanceD < this.Speed)
+                    {
+                        path.Dequeue();
+                        this.Position = next.Position;
+                    }
+                    else
+                    {
+                        this.Position = NewLocation;
+                    }
+                }
             }
         }
-        public Vector2 CalcMove(Player player)
+        public Vector2 CalcMove(Node Next)
         {
-            if (path != null && path.Count != 0)
+
+            Node next = path.Peek();
+
+            Vector2 transformation = new Vector2(0, 0);
+
+            if (next.Position.X < this.Position.X)
             {
-                
-
-
-                Node next = path.Peek();
-                
-                Vector2 transformation = new Vector2(0, 0);
-
-                Vector2 distanceV = next.Position - this.Position;
-                double distanceD = Math.Sqrt(Math.Pow(distanceV.X, 2) +  Math.Pow(distanceV.Y, 2));
-                if (distanceD < this.Speed)
-                {
-                    transformation = distanceV;
-                    path.Dequeue();
-                    return transformation;
-                }
-
-                if (next.Position.X < this.Position.X)
-                {
-                    transformation.X -= 1;
-                }
-                if (next.Position.X > this.Position.X)
-                {
-                    transformation.X += 1;
-                }
-                if (next.Position.Y < this.Position.Y)
-                {
-                    transformation.Y -= 1;
-                }
-                if (next.Position.Y > this.Position.Y)
-                {
-                    transformation.Y += 1;
-                }
-                if (transformation != Vector2.Zero)
-                {
-                    transformation.Normalize();
-                }
-                transformation = transformation * Speed; 
-
-                return transformation;
+                transformation.X -= 1;
             }
+            if (next.Position.X > this.Position.X)
+            {
+                transformation.X += 1;
+            }
+            if (next.Position.Y < this.Position.Y)
+            {
+                transformation.Y -= 1;
+            }
+            if (next.Position.Y > this.Position.Y)
+            {
+                transformation.Y += 1;
+            }
+            if (transformation != Vector2.Zero)
+            {
+                transformation.Normalize();
+            }
+            transformation = transformation * Speed;
+
+            return transformation;
+
             return new Vector2(this.Position.X, this.Position.Y);
         }
         public void DrawPath(SpriteBatch spriteBatch, Texture2D pixel)
